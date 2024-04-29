b8ba3717e30d248d54dccb50906bb036c38511bf
ruuiisu <luis.gjokaj@gmail.com>
2024-04-29 14:09:03 +0200

    ahhhouhhhsfixs

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,15 +4,15 @@ require('maps')
 require('plugins')
 
 local has = function(x)
-  return vim.fn.has(x) == 1
+    return vim.fn.has(x) == 1
 end
 
 local is_mac = has "macunix"
 local is_win = has "win32"
 
 if is_mac then
-  require('macos')
+    require('macos')
 end
-if is_win then 
-  require('windows')
+if is_win then
+    require('windows')
 end
